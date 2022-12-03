75942f12f311050acf8f74ce3adff49de64c4f32
Charles Farris <charlesfarris71@gmail.com>
2022-12-03 16:43:43 -0700

    Add Domain project
    - Add tests
    - Add package references

---
+++ src/Domain/Profession.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Immutable;
+using SleepingBearSystems.Tools.DataOriented;
+
+namespace SleepingBearSystems.CraftingTools.Domain;
+
+/// <summary>
+/// Methods used to support profession data.
+/// </summary>
+public static class Profession
+{
+    /// <summary>
+    /// Creates a profession record.
+    /// </summary>
+    /// <param name="id">The ID of the profession.</param>
+    /// <param name="name">The name of the profession.</param>
+    /// <returns>A record containing the profession.</returns>
+    public static ImmutableDictionary<string, object> CreateProfession(string id, string name) =>
+        Generic.BeginRecord()
+            .AddProperty("id", id)
+            .AddProperty("name", name)
+            .End();
+}
---
+++ tests/Domain.Tests/ProfessionTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Immutable;
+using SleepingBearSystems.Tools.DataOriented;
+
+namespace SleepingBearSystems.CraftingTools.Domain.Tests;
+
+/// <summary>
+/// Tests for <see cref="Profession"/>.
+/// </summary>
+internal static class ProfessionTests
+{
+    [Test]
+    public static void CreateProfession_ValidatesBehavior()
+    {
+        var record = Profession.CreateProfession("1234", "Cooking");
+        Assert.That(record, Is.Not.Null);
+        Assert.That(record, Is.TypeOf<ImmutableDictionary<string, object>>());
+        Assert.Multiple(() =>
+        {
+            Assert.That(record.Get("id"), Is.EqualTo("1234"));
+            Assert.That(record.Get("name"), Is.EqualTo("Cooking"));
+        });
+    }
+}
